873824164c27f0e6ebb507986ec6f57e624be035
PETRUS CHRIST RYAN <135100701+effatastore@users.noreply.github.com>
2023-08-18 14:20:47 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -121,7 +121,7 @@ echo "$localip $(hostname)" >> /etc/hosts
 fi
 mkdir -p /etc/xray
 
-echo -e "${tyblue} Welcome To Cobek Sawit Tunnel${NC} "
+echo -e "${tyblue} Welcome To EFFATA STORE SCRIPT${NC} "
 sleep 2
 echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
